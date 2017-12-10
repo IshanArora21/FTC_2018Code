8dfb11c2a2e48fd9e6a3ac3890b1b788db9d5460
bhargavilanka <31581853+bhargavilanka@users.noreply.github.com>
2017-12-09 22:03:08 -0500

    12/9/2017

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicManipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicManipulator.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
  * @version 1.0
- * @author (put your names here)
+ * @author harshpadhye, bhargavilanka
  * Scissor lift and claw combo
  */
 public class RelicManipulator {
@@ -21,23 +21,79 @@ public class RelicManipulator {
     //possible states the system could be in
     private systemStates possState;
     private enum systemStates {
-        SYSTEM_OFF
+        SYSTEM_OFF, EXTEND, RETRACT, GRAB, RELEASE
     };
 
+    //variables
+    private DcMotor slideMotor;
+    private Servo clawFingers;
+    private Servo clawWrist;
+
     public RelicManipulator(HardwareMap hwMap) {
+        //instantiate variables
+        slideMotor = new DcMotor(DcMotor.class, "slideMotor");
+        clawFingers = new Servo(Servo.class, "clawFingers");
+        clawWrist = new Servo(Servo.class, "clawWrist");
         possState = systemStates.SYSTEM_OFF;
     }
 
+    /**
+     * Runs appropriate motors based on
+     * current systme state
+     */
     public void update() {
         switch (possState) {
             case SYSTEM_OFF:
+                break;
+            case EXTEND:
+                slideMotor.setPower(1);
+                break;
+            case RETRACT:
+                slideMotor.setPower(-1);
+                break;
+            case GRAB:
+                //close, then lift
+                /*
+                    SERVO POSITIONAL VALUES
+                    SUBJECT TO CHANGE
+                    AKA WE HAVE NO IDEA
+                    UNTIL IT'S BUILT
+                 */
+                clawFingers.setPosition(1);
+                clawWrist.setPosition(0);
+                break;
+            case RELEASE:
+                //drop, then open
+                clawWrist.setPosition(1);
+                clawFingers.setPosition(0);
                 break;
             default:
                 System.out.println("Yo, you messed up, the system switch for Relic Manipulator went to default");
         }
     }
 
+    /**
+     * changes system states
+     */
+
     public void setStop() {
         possState = systemStates.SYSTEM_OFF;
     }
+
+    public void setExtended() {
+        possState = systemStates.EXTEND;
+    }
+
+    public void setRetract() {
+        possState = systemStates.RETRACT;
+    }
+
+    public void setGrab() {
+        possState = systemStates.GRAB;
+    }
+
+    public void setRelease() {
+        possState = systemStates.RELEASE;
+    }
+
 }
