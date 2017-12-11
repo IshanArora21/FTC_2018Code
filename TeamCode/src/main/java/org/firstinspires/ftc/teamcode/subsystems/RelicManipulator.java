ba64e182216abaedfe140cec6035061d535d118d
Harsh Padhye <harshpadhye@gmail.com>
2017-12-10 22:10:24 -0500

    Changed RelicManipulator
    cleaned code, added javaDocs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicManipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicManipulator.java
@@ -1,76 +1,76 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
  * @version 1.0
- * @author harshpadhye, bhargavilanka
- * Scissor lift and claw combo
+ * @author Harsh_Padhye
+ * Slide and claw
  */
 public class RelicManipulator {
 
     //singleton
     private static RelicManipulator relicManipulator = null;
     public static RelicManipulator getNewRelicManipulatorInstance(HardwareMap hwMap) {
-     if(relicManipulator == null) {
-         relicManipulator = new RelicManipulator(hwMap);
-     }
-     return relicManipulator;
+        if(relicManipulator == null) {
+            relicManipulator = new RelicManipulator(hwMap);
+        }
+        return relicManipulator;
     }
 
-    //possible states the system could be in
+    //servo and motor variables
+    private DcMotor slideMotor;
+    private Servo fingerServo;
+    private Servo wristServo;
+
+    //possible states the slide/claw could be in
     private systemStates possState;
     private enum systemStates {
         SYSTEM_OFF, EXTEND, RETRACT, GRAB, RELEASE
     };
 
-    //variables
-    private DcMotor slideMotor;
-    private Servo clawFingers;
-    private Servo clawWrist;
-
+    /**
+     * constructor
+     * @param hwMap : map of ports for motors, servos, etc
+     */
     public RelicManipulator(HardwareMap hwMap) {
-        //instantiate variables
-        //slideMotor = new DcMotor(DcMotor.class, "slideMotor");
-        slideMotor = hwMap.get(DcMotor.class, "slide_Motor");
-        //clawFingers = new Servo(Servo.class, "clawFingers");
-        clawFingers = hwMap.get(Servo.class, "claw_Fingers");
-        //clawWrist = new Servo(Servo.class, "clawWrist");
-        clawWrist = hwMap.get(Servo.class, "claw_Wrist");
+        //variables
+        slideMotor = hwMap.get(DcMotor.class, "slide");
+        fingerServo = hwMap.get(Servo.class, "finger");
+        wristServo = hwMap.get(Servo.class, "wrist");
+
+        //enum state
         possState = systemStates.SYSTEM_OFF;
     }
 
     /**
-     * Runs appropriate motors based on
-     * current systme state
+     * adjusts motors and servos to updated positions
+     * @param possState : current state of the mechanism
      */
     public void update() {
         switch (possState) {
             case SYSTEM_OFF:
+                slideMotor.setPower(0);
                 break;
             case EXTEND:
-                slideMotor.setPower(1);
+                //full power forward to extend slide
+                slideMotor.setPower(1.0);
                 break;
             case RETRACT:
-                slideMotor.setPower(-1);
+                //reverse power to retract slide
+                slideMotor.setPower(-1.0);
                 break;
             case GRAB:
-                //close, then lift
-                /*
-                    SERVO POSITIONAL VALUES
-                    SUBJECT TO CHANGE
-                    AKA WE HAVE NO IDEA
-                    UNTIL IT'S BUILT
-                 */
-                clawFingers.setPosition(1);
-                clawWrist.setPosition(0);
+                //close fingers, raise claw
+                fingerServo.setPosition(1.0);
+                wristServo.setPosition(0);
                 break;
             case RELEASE:
-                //drop, then open
-                clawWrist.setPosition(1);
-                clawFingers.setPosition(0);
+                //drop claw, open fingers
+                wristServo.setPosition(1.0);
+                fingerServo.setPosition(0);
                 break;
             default:
                 System.out.println("Yo, you messed up, the system switch for Relic Manipulator went to default");
@@ -78,27 +78,42 @@ public class RelicManipulator {
     }
 
     /**
-     * changes system states
+     * shuts down the system
      */
-
     public void setStop() {
         possState = systemStates.SYSTEM_OFF;
     }
 
-    public void setExtended() {
+    /**
+     * runs the arm until fully extended
+     * motor runs forward
+     */
+    public void setSlideExtend() {
         possState = systemStates.EXTEND;
     }
 
-    public void setRetract() {
+    /**
+     * pulls the arm back until fully retracted
+     * motor runs in reverse
+     */
+    public void setSlideRetract() {
         possState = systemStates.RETRACT;
     }
 
-    public void setGrab() {
+    /**
+     * grabs and raises the relic
+     */
+    public void setClawGrab() {
         possState = systemStates.GRAB;
     }
 
-    public void setRelease() {
+    /**
+     * lowers and releases relic
+     */
+    public void setClawRelease() {
         possState = systemStates.RELEASE;
     }
+
+
 
 }
